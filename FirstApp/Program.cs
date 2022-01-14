bfa6df58b6d3bb80345f55af70ccd621e69b7db3
Alexander Galichev <lex.galichev@gmail.com>
2022-01-15 04:14:23 +1000

    Added checked method for int to byte conversation.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -14,6 +14,7 @@ namespace FirstApp
             const byte ShoeSize = 41;
             double result = 10 % 3;
 
+
             Console.WriteLine("My name is " + MyName);
             Console.WriteLine("My age is {0}", Age);
             Console.WriteLine("Do I have a pet? {0}", HaveIAPet);
@@ -24,6 +25,14 @@ namespace FirstApp
             Console.WriteLine(MyFavoriteDay);
             Console.WriteLine("UShortMin = {0} UShortMax = {1}",ushort.MinValue, ushort.MaxValue);
             Console.WriteLine("10 % 3 = {0}", result);
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            int age =checked((byte) int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+
+
             Console.ReadKey();
 
 
